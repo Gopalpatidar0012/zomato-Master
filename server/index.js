53e493b0362064d996036d5f721d8b5fa4ac5d3c
Gopalpatidar0012 <yogapuran0012@gmail.com>
2022-01-18 16:11:09 +0530

    newone

--- server/index.js
+++ server/index.js
@@ -1,5 +1,3 @@
-
-
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";//import express package
@@ -13,16 +11,24 @@ import ConnectDB from "./database/connection.js";
 //API
 import Auth from "./API/Auth/index.js";
 
-const zomato = express();//declaring express application(using express)
-zomato.use(helmet());//this is package and provide additional security and secure our website.
-zomato.use(cors());//cross origin request ko allow karata h(we setup cors in both frontend and backend).
-zomato.use(express.json());//by the help of we (accept and send) the data in json formate.
-                           //JSON is most common way to transfer data in json formate on internet.
-                          //body parser middleware (it read json body request).
+const zomato = express();
+zomato.use(helmet());
+zomato.use(cors());
+zomato.use(express.json());
 
 //Application Route
 zomato.use("/auth",Auth)
 
+zomato.listen(8000, ()=>{
+   
+    ConnectDB()
+    .then(() =>{
+        console.log("server running");
+    })
+    .catch((err)=>{
+        console.log(" Server not connected......",err)
+    })
+});
 
 //  const { MongoClient } = require('mongodb');
 
@@ -41,105 +47,3 @@ zomato.use("/auth",Auth)
 // }).then(()=>{
 //     console.log("connection success");
 // }).catch((err)=>console.log(err));
-
-
-
-zomato.listen(8000, ()=>{
-    // console.log("runningggggg");
-    ConnectDB()
-    .then(() =>{
-        console.log("server runningggg");
-    })
-    .catch((err)=>{
-        console.log("not connected......",err)
-    })
-});
-
-// const app = express();
-
-
-/* 
-Route         /
-Description    get all user details 
-Access         PUBLIC
-Parameter      NONE
-Methods        get
-
-*/
-
-//http://localhost:3000/
-// app.get("/", async (req, res) => {
-//     return res.json({"Welcome":"To my Backend Software for the ZOMATO-MASTER"});
-// });
-
-
-
-/* 
-Route         /movies
-Description    get all user details
-Access         PUBLIC
-Parameter      NONE
-Methods        get
-
-*/
-
-//http://localhost:3000/movies
-// app.get("/movies", async (req, res) => {
-//     const getAllMovies = await MoviesModel.find(); //find() -> return array of object
-//     return res.json(getAllMovies);
-// });
-
-
-
-/* 
-Route         /movie-id
-Description    get single user details by id
-Access         PUBLIC
-Parameter      NONE
-Methods        get
-
-*/
-
-// http://localhost:5000/movie-id
-// app.get("/movie/:id",async (req, res) => {
-//     // console.log(req.params);
-//     const {id} = req.params;
-   
-//     console.log(id);
-//     const getSpecificMovies = await MoviesModel.findOne({_id: id}); //findOne()-> return only one object
-//     // console.log(getSpecificBook);
-//     // console.log(getSpecificBook.length);
-//     // if(getSpecificMovie===null) {
-//     //     return res.json({"error": `No Book found for the ISBN of ${id}`});
-//     // }
-//     return res.json(getSpecificMovies);
-// });
-
-
-
-
-
-/* 
-Route         /User-register
-Description    Post single user details in users collection
-Access         PUBLIC
-Parameter      NONE
-Methods        Post
-
-*/
-
-
-//http://localhost:3000/User-register
-// app.post("/user-register",async (req, res) => {
-//     //console.log(req.body);
-//     const addNewUser=await UserModel.create(req.body);
-   
-//     return res.json({
-//         UserAdded: addNewUser,
-//         message:"User was added"
-//     });
-// });
-
-// app.listen(5000, () => {
-//     console.log("MY EXPRESS APP IS RUNNING.....")
-// });
